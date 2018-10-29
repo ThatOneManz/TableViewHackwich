f02e67f171eb97e043f31603dbfcf41e71e1a54c
MIGUEL TAMAYO <rl237854@rlas-macair-32.local>
2018-10-29 12:10:09 -0500

    Initial Commit

---
+++ TableViewHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHackwich
+//
+//  Created by MIGUEL TAMAYO on 10/29/18.
+//  Copyright © 2018 MIGUEL TAMAYO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
